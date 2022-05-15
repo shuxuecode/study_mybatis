9f24c92c6c49443e745376aa32b1cbdf14c855ec
shuxuecode <zhaoshuxue@163.com>
2022-05-15 22:11:20 +0800

    commitByZsx

---
+++ mybatis-3-analysis/mybatis-demo-h2/src/main/java/mybatis/demo/h2/DemoMapper.java
@@ -0,0 +1,50 @@
+package mybatis.demo.h2;
+/*
+ *    Copyright 2009-2021 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+import mybatis.demo.h2.Demo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface DemoMapper {
+    List<Demo> selectByParam(@Param("search") Object search);
+
+    List<Demo> findAll();
+
+    List<Demo> findByUsername(String username);
+
+    List<String> selectDescriptionById(Integer id);
+
+    void createTable(String sql);
+
+    //List<String> selectDescriptionByConditions(Conditions conditions);
+    //List<String> selectDescriptionByConditions2(Conditions conditions);
+    //List<String> selectDescriptionByConditions3(Conditions conditions);
+    //
+    //class Conditions {
+    //  private Integer id;
+    //
+    //  public void setId(Integer id) {
+    //    this.id = id;
+    //  }
+    //
+    //  public Integer getId() {
+    //    return id;
+    //  }
+    //}
+
+}
---
+++ mybatis-3-analysis/mybatis-demo-h2/src/test/java/UnitTest2.java
@@ -0,0 +1,83 @@
+import com.alibaba.fastjson2.JSON;
+import mybatis.demo.h2.Demo;
+import mybatis.demo.h2.DemoMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ */
+public class UnitTest2 {
+
+    SqlSession sqlSession = null;
+
+    @BeforeEach
+    public void before() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("H2MybatisConfig.xml");
+
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        sqlSession = sqlSessionFactory.openSession();
+
+        sqlSession.getMapper(DemoMapper.class).createTable("");
+    }
+
+    @AfterEach
+    public void after() {
+        sqlSession.close();
+    }
+
+    @Test
+    public void t3() throws IOException {
+        //DemoMapper mapper = sqlSession.getMapper(DemoMapper.class);
+        //
+        //HashMap<String, Object> params = new HashMap<>();
+        //params.put("username", "a");
+        //params.put("ids", Lists.newArrayList(5, 6, 7));
+        //
+        //List<Demo> list = mapper.selectByParam(params);
+        //
+        //System.out.println(JSON.toJSONString(list));
+    }
+
+    @Test
+    public void t2() throws IOException {
+
+        DemoMapper mapper = sqlSession.getMapper(DemoMapper.class);
+
+        List<Demo> list = mapper.findAll();
+
+        System.out.println(JSON.toJSONString(list));
+
+    }
+
+    @Test
+    public void t1() throws IOException {
+
+        RowBounds rowBounds = new RowBounds(1, 2);
+
+        List<Demo> list = sqlSession.selectList("org.apache.ibatis.demo.DemoMapper.findAll", null, rowBounds);
+
+        System.out.println(JSON.toJSONString(list));
+    }
+
+    @Test
+    public void t() throws IOException {
+        List<Demo> list = sqlSession.selectList("org.apache.ibatis.demo.DemoMapper.findByUsername", "a");
+
+        System.out.println(JSON.toJSONString(list));
+    }
+
+
+}
